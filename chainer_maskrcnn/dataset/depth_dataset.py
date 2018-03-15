e45a0be931a5a9a0c8d02af7a0dfa20e41b90b49
katotetsuro <nistetsurooy@gmail.com>
2018-03-15 14:03:17 +0900

    add depth dataset loader

---
+++ chainer_maskrcnn/dataset/depth_dataset.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import chainer
+import numpy as np
+import cv2
+
+
+class DepthDataset(chainer.dataset.DatasetMixin):
+    def __init__(self, path, root='.'):
+        super().__init__()
+        with open(path, 'r') as f:
+            self.data = list(map(lambda x: x.strip(), f.readlines()))
+        self.root = root
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_example(self, index):
+        if index >= self.__len__():
+            raise IndexError
+
+        with np.load(Path(self.root).joinpath(self.data[index])) as f:
+            img = f['depth']
+            keypoints = f['keypoints']
+
+        if keypoints.shape[1] == 2:
+            visible = np.zeros((len(keypoints)))
+            visible.fill(2)
+            keypoints = np.concatenate((keypoints, visible), axis=1)
+        else:
+            keypoints[:, 2] = (keypoints[:, 2] > 0.2) * 2
+
+        assert keypoints.shape[1] == 3
+
+        if len(keypoints) > 20:
+            print(f'複数人が写っています {self.data[index]}')
+
+        # compute bounding box
+        x0 = np.min(keypoints[:, :2], axis=0) - [10, 10]
+        x1 = np.max(keypoints[:, :2], axis=0) + [10, 0]
+        bbox = np.concatenate([x0, x1]).reshape((1, 4))
+
+        return img, bbox, keypoints
