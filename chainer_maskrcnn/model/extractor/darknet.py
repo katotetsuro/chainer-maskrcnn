d919a3af179f515413cd0229a13d091083dd3160
katotetsuro <nistetsurooy@gmail.com>
2018-04-19 23:48:00 +0900

    to be able to select some of activation funcs.

--- chainer_maskrcnn/model/extractor/darknet.py
+++ chainer_maskrcnn/model/extractor/darknet.py
@@ -4,15 +4,16 @@ import chainer.functions as F
 
 
 class ConvBatch(chainer.Chain):
-    def __init__(self, out_channels, ksize, stride, pad):
+    def __init__(self, out_channels, ksize, stride, pad, activation):
         super().__init__()
         with self.init_scope():
             self.c = L.Convolution2D(in_channels=None, out_channels=out_channels,
                                      ksize=ksize, stride=stride, pad=pad)
             self.bn = L.BatchNormalization(size=out_channels)
+        self.activation = activation
 
     def __call__(self, x):
-        return F.leaky_relu(self.bn(self.c(x)))
+        return self.activation(self.bn(self.c(x)))
 
 
 class Darknet(chainer.Chain):
@@ -26,15 +27,15 @@ class Darknet(chainer.Chain):
     # anchor_sizes / anchor_base anchor_base is invisible from lamba function??
     anchor_scales = list(map(lambda x: x / 16., anchor_sizes))
 
-    def __init__(self):
+    def __init__(self, activation=F.relu):
         super().__init__()
 
         with self.init_scope():
-            self.conv1 = ConvBatch(16, 3, 1, 1)
-            self.conv2 = ConvBatch(32, 3, 1, 1)
-            self.conv3 = ConvBatch(64, 3, 1, 1)
-            self.conv4 = ConvBatch(128, 3, 1, 1)
-            self.conv5 = ConvBatch(256, 3, 1, 1)
+            self.conv1 = ConvBatch(16, 3, 1, 1, activation)
+            self.conv2 = ConvBatch(32, 3, 1, 1, activation)
+            self.conv3 = ConvBatch(64, 3, 1, 1, activation)
+            self.conv4 = ConvBatch(128, 3, 1, 1, activation)
+            self.conv5 = ConvBatch(256, 3, 1, 1, activation)
             # self.conv6 = ConvBatch(512, 3, 1, 1)
             # self.conv7 = ConvBatch(1024, 3, 1, 1)
         # anchor_sizes / anchor_base anchor_base is invisible from lamba function??
